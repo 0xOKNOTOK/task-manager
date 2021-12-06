039850bf8291fba90739b0efac6ec4f4d59a9fad
Ross Greenall <rossgreenall@gmail.com>
2021-12-06 15:43:57 +0000

    Created UTILS/ dir + installed firebase.

--- task/utils/firebase.ts
+++ task/utils/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB0pnBFv8Gpq6bFq77qwRO8t4ko754GVC8",
-  authDomain: "task-io-85855.firebaseapp.com",
-  projectId: "task-io-85855",
-  storageBucket: "task-io-85855.appspot.com",
-  messagingSenderId: "9985684064",
-  appId: "1:9985684064:web:ad84b3ba906cff9af70905",
-  measurementId: "G-X8VTVYHKCK"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
