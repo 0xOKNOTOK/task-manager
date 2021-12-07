e4c86ad0961fca6f921de33980ce253a876cdd6a
Ross Greenall <rossgreenall@gmail.com>
2021-12-07 23:14:58 +0000

    Brand component may become deprecated once the front-end is put together.

---
+++ task/components/Brand/Brand.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Brand.module.css'
+
+
+// THIS COMPONENT MAY BECOME REDUNDANT 
+
+
+export const Brand: React.FC = () => {
+    return <span>
+        <img />
+        <h1>task</h1>
+    </span>
+}
