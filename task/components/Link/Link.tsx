0364b967484b75891820fad92262d7b4b32dbc48
Ross Greenall <rossgreenall@gmail.com>
2021-12-07 23:10:50 +0000

    Props for link label: linkLabel and href: reference

--- task/components/Link/Link.tsx
+++ task/components/Link/Link.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styles from './Link.module.css'
-export const Link: React.FC = () => {
-    return <a></a>
+
+
+interface Props {
+    reference: string,
+    linkLabel: string,
+}
+
+export const Link: React.FC<Props> = (props) => {
+    return <a href={props.reference}>{props.linkLabel}</a>
 }
