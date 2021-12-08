d3e72e5dc0a4da50c3a77052ae60fbbc112a890d
Ross Greenall <rossgreenall@gmail.com>
2021-12-08 17:34:26 +0000

    MenuCard component will be displayed after navigating to a new page.

---
+++ task/components/MenuCard/MenuCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FlexWrapper } from '../FlexWrapper/FlexWrapper'
+import styles from './MenuCard.module.css'
+
+interface Props {
+    
+}
+
+const MenuCard = (props: Props) => {
+    return <FlexWrapper>
+        <h2>Team</h2>
+        <p>... ... ... ... ...</p>
+    </FlexWrapper>
+
+}
