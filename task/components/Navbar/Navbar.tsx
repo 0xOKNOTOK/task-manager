4e8bcfb58c853d28472779e4b27c0473e34f118a
Ross Greenall <rossgreenall@gmail.com>
2021-12-09 19:04:56 +0000

    Changed default font size and moved to rem units. Centered svg icons with text, styled brand on navbar

--- task/components/Navbar/Navbar.tsx
+++ task/components/Navbar/Navbar.tsx
@@ -5,37 +5,44 @@ import {MdPeopleAlt, MdSpaceDashboard, MdChat, MdArticle, MdPayments, MdAccountB
 
 export const Navbar = () => {
     return <nav className={styles.nav}>
-        <h1>{`> task`}</h1>
+        <h1><span className={styles.arrow}>{`> `}</span>task</h1>
         <ul className={styles.ul}>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdPeopleAlt className={styles.svg}/>} reference='/teams' linkLabel='Teams'/>
             </li>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdSpaceDashboard className={styles.svg}/>} reference='/boards' linkLabel='Boards' />
             </li>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdChat className={styles.svg}/>} reference='/chats' linkLabel='Chats' />
             </li>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdArticle className={styles.svg}/>} reference='/timetable' linkLabel='Timetable' />
             </li>
          </ul>
          <ul className={styles.ul}>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdPayments className={styles.svg}/>} reference='/plans' linkLabel='Plans' />
             </li>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdAccountBox className={styles.svg}/>} reference='/account' linkLabel='Account' />
             </li>
-            <li>
+            <li className={styles.li}>
                 <AnchorLink icon={<MdSettingsApplications className={styles.svg} />} reference='/settings' linkLabel='Settings' />
             </li>
          </ul>
-         <span>
-             BLANK
-         </span>
+        <span>
+        </span>
+        <span>
+        </span>
+        <span>
+        </span>
+        <span>
+        </span>
          <ul className={styles.ul}>
+             <li className={styles.li}>
              <AnchorLink icon={<MdLogout className={styles.svg}/>} reference='/logout' linkLabel='Log out' />
+             </li>
          </ul>
          
     </nav>
