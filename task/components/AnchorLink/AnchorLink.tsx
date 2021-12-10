7fd1a4d9aa1e44060e793565e5ab3042f500c1a0
Ross Greenall <rossgreenall@gmail.com>
2021-12-10 18:07:07 +0000

    Changed styling of anchor links.

--- task/components/AnchorLink/AnchorLink.tsx
+++ task/components/AnchorLink/AnchorLink.tsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 import Link from 'next/link'
 import styles from './AnchorLink.module.css'
 
-
 interface Props {
-    reference: string,
-    linkLabel: string,
-    icon?: any,
+  reference: string
+  linkLabel: string
+  icon?: any
 }
 
-export const AnchorLink = ({reference, linkLabel, icon}: Props) => {
-    return <Link href={reference} >
-        <a className={styles.base}>
-            {icon ? icon : null}
-            {linkLabel}
-        </a>
+export const AnchorLink = ({ reference, linkLabel, icon }: Props) => {
+  return (
+    <Link href={reference}>
+      <a className={styles.base}>
+        {icon ? icon : null}
+        <p>{linkLabel}</p>
+      </a>
     </Link>
+  )
 }
