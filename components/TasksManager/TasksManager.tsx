d2571d49c0ca8860c9d508bdd3d3f65fb0fbc1ea
Ross Greenall <rossgreenall@gmail.com>
2021-12-11 20:14:51 +0000

    Started integrating grid to display tasks evenly.

--- components/TasksManager/TasksManager.tsx
+++ components/TasksManager/TasksManager.tsx
@@ -8,13 +8,16 @@ export const TasksManager = () => {
     <GridWrapper className={styles.grid}>
       <div className={styles.columnTitles}>
         <SectionHeader sectionTitle='To-do' dotColour='#C4C4C4' />
+      </div>
+      <div className={styles.columnTitles}>
         <SectionHeader sectionTitle='In Progress' dotColour='#11A6A6' />
+      </div>
+      <div className={styles.columnTitles}>
         <SectionHeader sectionTitle='In Review' dotColour='#F9896A' />
+      </div>
+      <div className={styles.columnTitles}>
         <SectionHeader sectionTitle='Completed' dotColour='#21DE1D' />
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
     </GridWrapper>
   )
 }
