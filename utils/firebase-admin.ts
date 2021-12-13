2f07f492113ca14fdac5917ec2f75bdea13e70e2
Ross Greenall <rossgreenall@gmail.com>
2021-12-13 13:11:35 +0000

    Created admin credentials for firebase.

--- utils/firebase-admin.ts
+++ utils/firebase-admin.ts
@@ -1,3 +1,13 @@
 import FirebaseAdmin from 'firebase-admin'
 
-import serviceAccount from '../sec.json'
+import { serviceAccount } from './sec'
+
+if (!FirebaseAdmin.apps.length) {
+  FirebaseAdmin.initializeApp({
+    credential: FirebaseAdmin.credential.cert({
+      privateKey: serviceAccount.private_key,
+      clientEmail: serviceAccount.client_email,
+      projectId: serviceAccount.project_id
+    })
+  })
+}
