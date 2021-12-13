7fe69b89d3ed391721ec4be5ff772a4bde7baeee
Ross Greenall <rossgreenall@gmail.com>
2021-12-13 22:47:32 +0000

    Styled login page.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,45 +1,48 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import { firebaseClient } from '../utils/firebaseClient'
+import styles from '../styles/Login.module.css'
 
 const Login = (_props: any) => {
   const [email, setEmail] = useState('')
   const [pass, setPass] = useState('')
   return (
-    <div>
-      <Link href='/'>
-        <a>Go back to home page</a>
-      </Link>
-      <br />
-      <input
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-        placeholder={'Email'}
-      />
-      <input
-        type={'password'}
-        value={pass}
-        onChange={e => setPass(e.target.value)}
-        placeholder={'Password'}
-      />
-      <button
-        onClick={async () => {
-          await firebaseClient
-            .auth()
-            .createUserWithEmailAndPassword(email, pass)
-          window.location.href = '/'
-        }}
-      >
-        Create account
-      </button>
-      <button
-        onClick={async () => {
-          await firebaseClient.auth().signInWithEmailAndPassword(email, pass)
-          window.location.href = '/'
-        }}
-      >
-        Log in
-      </button>
+    <div className={styles.wrapper}>
+      <h1>
+        <span>{`> `}</span>task
+      </h1>
+      <div className={styles.login}>
+        <h3>Account Login</h3>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder={'Email'}
+        />
+        <input
+          type={'password'}
+          value={pass}
+          onChange={e => setPass(e.target.value)}
+          placeholder={'Password'}
+        />
+        <button
+          onClick={async () => {
+            await firebaseClient
+              .auth()
+              .createUserWithEmailAndPassword(email, pass)
+            window.location.href = '/'
+          }}
+        >
+          Create account
+        </button>
+        <button
+          onClick={async () => {
+            await firebaseClient.auth().signInWithEmailAndPassword(email, pass)
+            window.location.href = '/'
+          }}
+        >
+          Log in
+        </button>
+      </div>
     </div>
   )
 }
