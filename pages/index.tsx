3a59bb7b63a37b9ab035be60e6395924a9977054
Ross Greenall <rossgreenall@gmail.com>
2021-12-17 20:09:14 +0000

    Removed img from index, styled features further, seperated landing into div.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import styles from '../styles/Home.module.css'
 
 // Components
 import { Button } from '../components/Button/Button'
-import { Footer } from '../components/Footer/Footer'
+import { TiGroup } from 'react-icons/ti'
+import { GoMarkdown } from 'react-icons/go'
+import { MdMarkChatUnread } from 'react-icons/md'
 import { FlexWrapper } from '../components/FlexWrapper/FlexWrapper'
 import { Header } from '../components/Header/Header'
 import { AnchorLink } from '../components/AnchorLink/AnchorLink'
@@ -35,32 +37,36 @@ const Home: NextPage = () => {
           ></AnchorLink>
         </Header>
         <FlexWrapper className={styles.flex}>
-          <h1>{`Task management for your projects.`}</h1>
-          <h4>{`Card based management system designed to speed up development within small teams.`}</h4>
-          <Button buttonLabel={'Login >'} onClick={() => 1}></Button>
-        </FlexWrapper>
-        <FlexWrapper className={styles.flex}>
-          <></>
-        </FlexWrapper>
-        <FlexWrapper className={styles.features}>
-          <div className={styles.feature}>
-            <h5>Work Together</h5>
-            <p>
-              Create teams with members to begin working on a new project
-              together.
-            </p>
+          <div className={styles.landing}>
+            <h1>{`Task management for your projects.`}</h1>
+            <h4>{`Card based management system designed to speed up development within small teams.`}</h4>
+            <Button buttonLabel={'Login >'} onClick={() => 1}></Button>
           </div>
-          <div className={styles.feature}>
-            <h5>Markdown Styling</h5>
-            <p>
-              Built in markdown stying for increased organisation on individual
-              tasks.
-            </p>
-          </div>
-          <div className={styles.feature}>
-            <h5>Communcation built in</h5>
-            <p>Built in discord support, live chat and comment system.</p>
-          </div>
+          <FlexWrapper className={styles.features}>
+            <div className={styles.feature}>
+              <TiGroup />
+              <h5>Work Together</h5>
+              <p>
+                Create teams with members to begin working on a new project
+                together.
+              </p>
+            </div>
+            {'>_'}
+            <div className={styles.feature}>
+              <GoMarkdown />
+              <h5>Markdown Styling</h5>
+              <p>
+                Built in markdown stying for increased organisation on
+                individual tasks.
+              </p>
+            </div>
+            {'>_'}
+            <div className={styles.feature}>
+              <MdMarkChatUnread />
+              <h5>Communcation built in</h5>
+              <p>Built in discord support, live chat and comment system.</p>
+            </div>
+          </FlexWrapper>
         </FlexWrapper>
       </main>
     </div>
